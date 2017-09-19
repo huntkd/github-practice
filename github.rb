f36da725da934b576b78911c0a6357030aaa172e
Keelin Hunt <huntkd@s.dcsdk12.org>
2017-09-19 10:46:00 -0600

    Name test

--- github.rb
+++ github.rb
@@ -1,1 +1,16 @@
- puts "Hello Github! Octocats are amazing"
+
+puts "Hello Github! Octocats are amazing"
+
+puts "Keelin"
+puts "Nithya"
+puts "Elijah"
+puts "Nikki"
+puts "Mihir"
+puts "Jett"
+puts "Motts"
+puts "Bryce"
+puts "Derek"
+puts "Charlie"
+puts "David"
+puts "Ben"
+puts "Mr. Padjen"
